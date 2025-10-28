06ff35646d2a32f341af8cf5ce65e9e5083aa6fa
Vatsal Kumar Singh <vatsalarjun14@gmail.com>
2025-10-28 23:04:08 +0530

    Update imports and maintain vector DB creation logic

--- build_db.py
+++ build_db.py
@@ -1,8 +1,8 @@
 import os
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 def create_vector_db():
